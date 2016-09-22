5980aa9b083cb7fadbbbd6b4784ef34a3f3f2f17
shauntc <shaun@shauntc.com>
2016-09-22 12:58:53 -0700

    Initial Commit

---
+++ CoverFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlowLayout
+//
+//  Created by Shaun Campbell on 2016-09-22.
+//  Copyright © 2016 Shaun Campbell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
